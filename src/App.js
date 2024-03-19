3434d9cbeecb22e7c97ab266dc471d8c1def15da
Vinayak Prakash <vinayakprakash2121@gmail.com>
2024-03-19 17:37:26 +0530

    s

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@
 function App() {
 
 let count = 0;
+const addcount = ()=>{
+  count=count+1
+  console.log(count)
+}
   return (
-
+<div>
+  <h1>
+    COUNT: {count}
+  </h1>
+  <button onClick={addcount}>CLICK ME</button>
+</div>
   );
 }
 
